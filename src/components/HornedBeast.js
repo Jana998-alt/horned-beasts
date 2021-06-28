3fddfac8766e477e1cb4e66ba8c5f6037c46f192
Jana Abdelkader <janaosama1300@gmail.com>
2021-06-28 23:06:32 +0300

    finished task 2

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import reactDom from 'react-dom';
 
+
 class HornedBeast extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {favorited: 0}
+    
+    }
+
+    favorate =() => {
+        this.setState({favorited : this.state.favorited + 1}) 
+    }
+
     render(beastTitle,ImageURL,imageTitle,description,noOfHorns) {
         return (
             <div>
                 <h2>{this.props.beastTitle}</h2>
-                <img src={this.props.ImageURL} alt={this.props.imageTitle} title = {this.props.imageTitle}/>
+                <img src={this.props.ImageURL} alt={this.props.imageTitle} title = {this.props.imageTitle} onClick={this.favorate} />
                 <p>{this.props.description}</p>
+                <p>🖤 {this.state.favorited}</p>
             </div>
         )
     }
