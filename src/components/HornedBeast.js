7b274444b13752eb5617faafab352520d30aa11d
Jana Abdelkader <janaosama1300@gmail.com>
2021-06-28 21:15:26 +0300

    task 1

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -2,7 +2,7 @@ import React from 'react';
 import reactDom from 'react-dom';
 
 class HornedBeast extends React.Component {
-    render(beastTitle,ImageURL,imageTitle,description) {
+    render(beastTitle,ImageURL,imageTitle,description,noOfHorns) {
         return (
             <div>
                 <h2>{this.props.beastTitle}</h2>
